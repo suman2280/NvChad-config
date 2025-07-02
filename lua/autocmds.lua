fdb96cda3d61381fcb90dfc41ef476c57e1a377b
Suman Sardar <sudarshansardar22@gmail.com>
2025-07-02 12:07:52 +0530

    Added some formatting for html

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,10 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "html",
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
